ea190b1c8310538552941058b97f3cfd9ffe85ea
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-11-15 20:15:03 +0000

    index last version of

--- index1.c
+++ index1.c
@@ -65,6 +65,7 @@ int pointers();
 int pointersto();
 int pointersfrom();
 int ford();
+int dowhile();
 FILE *f1;
 FILE *f2;
 
@@ -151,6 +152,7 @@ void readll(char *argv1){
 	if (n==94) function(ss[1]);
 	if (n==95) pointersto();
 	if (n==96) pointersfrom();
+	if (n==97) dowhile();
 	if (n>=substart) callfunction(ss[0]);
 	//printf("**%d\n",n);
 	if (error==1){
@@ -2154,6 +2156,7 @@ void head(){
 		addkey ("function",2); //94
 		addkey ("pointer.to",3); //95
 		addkey ("pointer.from",3); //96
+		addkey ("while",8); //97
 		varsart=cursor;
 		substart=subcursor;
 }
@@ -2927,6 +2930,108 @@ int ford(){
 		fprintf(f2,"	mov ebx,[si]\n");
 		fprintf(f2,"	cmp eax,ebx\n");
 		fprintf(f2,"	jle lineno%d \n",lineno);
+		
+	}
+	
+}
+
+
+
+//=================================================================
+
+//=================================================================
+
+int dowhile(){
+	int i;
+	int i1;
+	int i2;
+	int i3;
+	int i4;
+	int i5;
+	int i6;
+	int i7;
+	int i8;
+	int i9;
+	char *ss1;
+	if(8==count){
+
+		error=0;
+
+		ss1=uppercase(ss[1]);
+		i1=findvar(ss1);
+		if (i1==-1){
+			printf("error var1\n");
+			error=1;
+		}
+
+		ss1=uppercase(ss[2]);
+		i2=findvar(ss1);
+		if (i2==-1){
+			printf("error var2\n");
+			error=1;
+		}
+
+		ss1=uppercase(ss[5]);
+		i5=findvar(ss1);
+		if (i5==-1){
+			printf("error var3\n");
+			error=1;
+		}
+
+		ss1=uppercase(ss[4]);
+		i4=findvar(ss1);
+		if (i4==-1){
+			printf("error var4\n");
+			error=1;
+		}
+
+
+		ss1=uppercase(ss[3]);
+		i3=findkey(ss1);
+		if (i3<substart){
+			printf("error key\n");
+			error=1;
+		}
+
+
+
+		ss1=uppercase(ss[6]);
+		i6=findvar(ss1);
+		if (i6==-1){
+			printf("error var6\n");
+			error=1;
+		}
+
+		ss1=uppercase(ss[7]);
+		i7=findvar(ss1);
+		if (i7==-1){
+			printf("error var7\n");
+			error=1;
+		}
+
+
+		fprintf(f2,"	lineno%d:\n",lineno);
+		fprintf(f2,"	mov si,varnext%d\n",i4+varnextstart);
+		fprintf(f2,"	cs\n");
+		fprintf(f2,"	mov eax,[si]\n");
+		fprintf(f2,"	mov si,varnext%d\n",i5+varnextstart);
+		fprintf(f2,"	cs\n");
+		fprintf(f2,"	mov ebx,[si]\n");
+		fprintf(f2,"	mov si,varnext%d\n",i6+varnextstart);
+		fprintf(f2,"	cs\n");
+		fprintf(f2,"	mov ecx,[si]\n");
+		fprintf(f2,"	mov si,varnext%d\n",i7+varnextstart);
+		fprintf(f2,"	cs\n");
+		fprintf(f2,"	mov edx,[si]\n");
+		fprintf(f2,"	call sub%d\n",i3);
+		fprintf(f2,"	mov si,varnext%d\n",i1+varnextstart);
+		fprintf(f2,"	cs\n");
+		fprintf(f2,"	mov eax,[si]\n");
+		fprintf(f2,"	mov si,varnext%d\n",i2+varnextstart);
+		fprintf(f2,"	cs\n");
+		fprintf(f2,"	mov ebx,[si]\n");
+		fprintf(f2,"	cmp eax,ebx\n");
+		fprintf(f2,"	jnz lineno%d \n",lineno);
 		
 	}
 	
